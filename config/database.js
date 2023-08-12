80dfdcc9947cee65ad8bac2eb96880492f46d379
Eby-Tom <ebytomy7@gmail.com>
2023-08-12 20:11:34 +0530

    updates

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ var db = null
 
 module.exports.connectDB = async () => {
     mongoose.set('strictQuery', true)
-    const dbinstance = await mongoose.connect(process.env.MONGODB_URL);
+    const dbinstance = await mongoose.connect("mongodb+srv://ebytomy7:uxgUJ5le97ZXk9gA@cluster.odk5ymv.mongodb.net/?retryWrites=true&w=majority");
     console.log("Database Connected")
     db = dbinstance
     return dbinstance;
